be377e9bbf1ed67f1a96527ce45f445dc9d4a503
yrmallu <yedukondalareddy@gmail.com>
2019-03-22 18:02:06 +0000

    adding credentials

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,7 +3,7 @@ ShopifyApp.configure do |config|
   config.api_key = "a4f6725c797ab3df754fa8cfe625b233"
   config.secret = "26454a5cd590d2de9da2ae392c716fe6"
   config.scope = "read_products, read_orders, read_customers, write_orders, read_shipping, write_shipping, read_fulfillments, write_fulfillments" # Consult this page for more scope options:
-  config.embedded_app = true
+  config.embedded_app = false
   config.after_authenticate_job = false
   config.session_repository = Shop
 end
